6e4bdb11330ffa1dbc54e2791216dcd117af2fe9
kathan <kathan8794@gmail.com>
2023-10-07 00:37:56 +0530

    Add project files.

---
+++ E-Commercce/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commercce.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProduct()
+        {
+            return "All Product";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProductbyId(int id)
+        {
+            return "Product with id";
+        }
+    }
+}
